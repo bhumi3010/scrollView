81fbb05caf6a265add86d56a3dd8a67beb6c4ea1
CromosysMAC2 <cromosysmac2@Cromosys-Mac2.local>
2016-03-01 14:50:45 +0530

    first commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by CromosysMAC2 on 3/1/16.
+//  Copyright © 2016 cromosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by CromosysMAC2 on 3/1/16.
+//  Copyright © 2016 cromosys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
